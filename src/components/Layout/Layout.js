fdd2c8098f941c0bd729091a9397a7821af4f966
LikaGreb <lika.greben@gmail.com>
2022-10-25 16:46:39 +0300

    fixed switch

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 
+import { useContext } from 'react'
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import cn from 'classnames';
 import styles from "./Layout.module.scss";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 function Layout({ children }) {
-  
+  const { theme } = useContext(ThemeContext)
   return (
-    <div className={styles.body}>
+    <div className={`${styles.body} ${styles[theme]}`}>
       <Header />
       <section>
         <div className="container">{children}</div>
--- src/providers/ThemeProvider.js
+++ src/providers/ThemeProvider.js
@@ -2,22 +2,20 @@ import { useState, useEffect } from 'react';
 import { ThemeContext, themes } from '../contexts/ThemeContext';
 
 const getTheme = () => {
-  const theme = `${window?.localStorage?.getItem('theme')}`
-  if (Object.values(themes).includes(theme)) return theme;
+  const theme = window.localStorage.getItem('theme')
+  if (Object.values(themes).includes(theme)) {
+    return theme;
+  };
 
-  const userMedia = window.matchMedia('(prefers-color-scheme: light)');
-  if (userMedia.matches) return themes.light;
-
-  return themes.dark;
+  return themes.light;
 }
 
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(getTheme);
 
   useEffect(() => {
-    document.documentElement.dataset.theme = theme;
     localStorage.setItem('theme', theme);
-  }, [ theme ])
+  }, [theme])
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
